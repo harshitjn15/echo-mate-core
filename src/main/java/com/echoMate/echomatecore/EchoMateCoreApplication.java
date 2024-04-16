b87b75a73b84881fb9099e44b8fd8ec13274c01e
Harshit <harshitl@lentra.ai>
2024-04-16 23:42:17 +0530

    Initial commit

---
+++ src/main/java/com/echoMate/echomatecore/EchoMateCoreApplication.java
@@ -0,0 +1,13 @@
+package com.echoMate.echomatecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoMateCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoMateCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echoMate/echomatecore/EchoMateCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoMate.echomatecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoMateCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
