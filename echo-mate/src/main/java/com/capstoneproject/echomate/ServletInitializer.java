b739c0e8e3b89bec1d15e5b9e0ce6e2fd3fe6e75
Harshit <harshit.jn15@gmail.com>
2024-05-12 17:05:03 +0530

    Project Setup

---
+++ echo-mate/src/main/java/com/capstoneproject/echomate/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.echomate;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EchoMateApplication.class);
+    }
+
+}
