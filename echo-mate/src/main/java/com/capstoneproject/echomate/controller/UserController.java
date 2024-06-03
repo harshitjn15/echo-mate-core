95398a50b00cc666486eedf5ddd224237b9be4c5
Harshit <harshit.jn15@gmail.com>
2024-06-04 00:45:22 +0530

    Created Login API Added WebConfig for Cross-Origin

--- echo-mate/src/main/java/com/capstoneproject/echomate/controller/UserController.java
+++ echo-mate/src/main/java/com/capstoneproject/echomate/controller/UserController.java
@@ -3,8 +3,12 @@ package com.capstoneproject.echomate.controller;
 import com.capstoneproject.echomate.model.User;
 import com.capstoneproject.echomate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -17,6 +21,25 @@ public class UserController {
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody User user) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            Optional<User> userData = userService.findByUsername(user.getUsername());
+            User userDetails = userData.orElseThrow(() -> new Exception("User not found"));
+
+            if (!user.getPassword().equals(userDetails.getPassword())) {
+                throw new Exception("Wrong password");
+            }
+            response.put("status", "success");
+            response.put("message", "Login successful");
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            response.put("status", "error");
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);        }
     }
 
     @GetMapping("/{username}")
