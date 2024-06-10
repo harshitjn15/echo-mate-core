439c209fd8b17f87480d9e68a4e8bc1e026d7218
Harshit <harshit.jn15@gmail.com>
2024-06-10 13:25:48 +0530

    changes for Mongo Atlas

--- echo-mate/src/main/java/com/capstoneproject/echomate/config/WebConfig.java
+++ echo-mate/src/main/java/com/capstoneproject/echomate/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.capstoneproject.echomate;
+package com.capstoneproject.echomate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
